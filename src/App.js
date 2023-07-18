85746a46556c806adf99eda5730eb3f1f544b37a
Julia Loftin <julia.loftin7@gmail.com>
2023-07-17 19:03:00 -0500

    adds satData imports

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import Banner from "./components/Banner";
 import Buttons from "./components/Buttons";
 import Table from "./components/Table";
+import useState from "react";
+import satData from "./components/satData";
 
 
 function App() {
